c76fb8ae82d8c8a2a6a5831318c2ae5d38950fe0
junhup <junhup.lim@u.nus.edu>
2020-11-19 22:00:20 +0800

    add gesture for thumbs up and victory

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,16 @@ import * as tf from "@tensorflow/tfjs";
 import * as handpose from "@tensorflow-models/handpose";
 import Webcam from "react-webcam";
 import { drawHand } from "./hand";
+import * as fp from "fingerpose";
+import victory from "../victory.png";
+import thumbs_up from "../thumbs_up.png";
 
 export default function Home() {
   const webcamRef = useRef(null);
   const canvasRef = useRef(null);
 
+  const [emoji, setEmoji] = useState(null);
+  const images = { thumbs_up: thumbs_up, victory: victory };
 
   const runHandpose = async () => {
     const net = await handpose.load();
@@ -40,7 +45,26 @@ export default function Home() {
 
       // Make Detections
       const hand = await net.estimateHands(video);
-      console.log(hand);
+
+      // Gesture handling
+      if (hand.length > 0) {
+        const GE = new fp.GestureEstimator([
+          fp.Gestures.VictoryGesture,
+          fp.Gestures.ThumbsUpGesture,
+        ]);
+        const gesture = await GE.estimate(hand[0].landmarks, 4);
+        if (gesture.gestures !== undefined && gesture.gestures.length > 0) {
+          // console.log(gesture.gestures);
+
+          const confidence = gesture.gestures.map(
+            (prediction) => prediction.confidence
+          );
+          const maxConfidence = confidence.indexOf(
+            Math.max.apply(null, confidence)
+          );
+          setEmoji(gesture.gestures[maxConfidence].name);
+        }
+      }
 
       // Draw mesh
       const ctx = canvasRef.current.getContext("2d");
@@ -81,5 +105,24 @@ export default function Home() {
           height: 480,
         }}
       />
-    </div>);
+
+      {emoji !== null ? (
+        <img
+          src={images[emoji]}
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 400,
+            bottom: 500,
+            right: 0,
+            textAlign: "center",
+            height: 100,
+          }}
+        />
+      ) : (
+        ""
+      )}
+    </div>
+  );
 }
