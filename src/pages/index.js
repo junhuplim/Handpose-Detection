d73ddff42146ff1987bb36815c0969b0fd130ce6
junhup <junhup.lim@u.nus.edu>
2020-11-19 21:38:29 +0800

    add react webcam and canvas

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,42 @@
-import React from "react"
+import React, { useRef, useState, useEffect } from "react";
+import * as tf from "@tensorflow/tfjs";
+import * as handpose from "@tensorflow-models/handpose";
+import Webcam from "react-webcam";
 
 export default function Home() {
-  return <div>Hello world!</div>
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
+
+  return (
+    <div>
+      <Webcam
+        ref={webcamRef}
+        style={{
+          position: "absolute",
+          marginLeft: "auto",
+          marginRight: "auto",
+          left: 0,
+          right: 0,
+          textAlign: "center",
+          zindex: 9,
+          width: 640,
+          height: 480,
+        }}
+      />
+
+      <canvas
+        ref={canvasRef}
+        style={{
+          position: "absolute",
+          marginLeft: "auto",
+          marginRight: "auto",
+          left: 0,
+          right: 0,
+          textAlign: "center",
+          zindex: 9,
+          width: 640,
+          height: 480,
+        }}
+      />
+    </div>);
 }
